c585cf9e2e6f66942709d4844997390a4b856d59
Ash Wolford <piro@live.com>
2020-03-02 16:04:20 -0600

    cleanup

--- windows.js
+++ windows.js
@@ -2,18 +2,14 @@ if (process.platform !== "win32")
     return
     
 const { readFileSync } = require("fs-extra")
-
 const { startInBackground } = JSON.parse(readFileSync("./config.json"))
 
 const { NotifyIcon, Icon, Menu } = require("not-the-systray");
 
-const win32 = require("win32-api")
-const { Kernel32, User32, ShowWindowArgs } = win32
-const knl32 = Kernel32.load()
+const { User32 } = require("win32-api")
 const user32 = User32.load()
 
-const ref = require("ref-napi")
-
+// Will break if there are two windows with the same title
 const lpszWnd = Buffer.from("KCCacheProxy Console\0", "ucs2")
 const hWnd = user32.FindWindowExW(0, 0, null, lpszWnd)
 var hWndOk = typeof hWnd === 'number' && hWnd > 0
@@ -23,33 +19,36 @@ if (!hWndOk) {
     console.error("Could not retrieve console window handle.")    
 }
 
+// System tray icon
 var lastClickTime = 0
 const appIcon = new NotifyIcon({
     icon: Icon.load("icon.ico", Icon.small),
     tooltip: "KCCacheProxy",
     onSelect({ target, rightButton, mouseX, mouseY }) {
-        // manual doubleclick check because this api is a little dumb like that
-        if (!rightButton) {
-            var now = new Date().getTime()
-            if (now - lastClickTime < 400) {
-                showWindow()
-                lastClickTime = 0
-            }
-            lastClickTime = now
+        if (rightButton) {
+            handleMenu(mouseX, mouseY);
         }
         else {
-            handleMenu(mouseX, mouseY);
+            // Manual doubleclick check because this API is a little basic
+            var now = new Date().getTime()
+            if (now - lastClickTime < 400) {
+                showWindow() // No parameter toggles between shown/hidden
+                lastClickTime = 0
+            }
+            else {
+                lastClickTime = now
+            }
         }
     }
 })
 
-// Remove the icon and return window to normal on exit/Ctrl-C
-process.on("SIGINT", () => {
+// Remove the icon and return window to normal when closing
+process.on("SIGINT", () => { // Ctrl-C
     appIcon.remove()
     showWindow(SW_SHOWNORMAL)
     process.exit()
 })
-process.on("exit", () => {
+process.on("exit", () => { // process.exit, etc.
     appIcon.remove()
     showWindow(SW_SHOWNORMAL)
 })
@@ -59,6 +58,8 @@ async function handleMenu(x, y) {
     const hideId = 2
     const exitId = 1
     var menuItems = []
+    // Only show the appropriate menu option.
+    // Alternatively, change to a single 'Show/hide' and use ShowWindow()
     if (windowState === SW_HIDE) {
         menuItems.push({ id: showId, text: "Show" })
     }
@@ -83,16 +84,19 @@ async function handleMenu(x, y) {
     }
 }
 
+
 const SW_HIDE = 0
 const SW_SHOWNORMAL = 1
 var windowState = SW_SHOWNORMAL;
 function showWindow(state) {
+    // If no state was provided, just toggle the current state
     if (state === undefined)
         state = windowState === SW_SHOWNORMAL ? SW_HIDE : SW_SHOWNORMAL
     windowState = state
     user32.ShowWindow(hWnd, state)
 }
 
+// Hide the window on startup based on config.json
 if (startInBackground) {
     showWindow(SW_HIDE)
 }
