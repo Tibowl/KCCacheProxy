839a411f406b776f297337660a50bcf1f27a4708
Ash Wolford <piro@live.com>
2020-03-02 17:52:36 -0600

    Native modules fix

--- build.sh
+++ build.sh
@@ -26,6 +26,12 @@ mv proxy-macos KCCacheProxy-macos/proxy-macos &&
 rm -r KCCacheProxy-macos &
 
 cp -r KCCacheProxy/ KCCacheProxy-win/ &&
+mkdir -p ./KCCacheProxy-win/node_modules/not-the-systray
+cp ../node_modules/not-the-systray/notify_icon.node ./KCCacheProxy-win/node_modules/not-the-systray/
+mkdir -p ./KCCacheProxy-win/node_modules/ref-napi/build/Release
+cp ../node_modules/ref-napi/build/Release/binding.node ./KCCacheProxy-win/node_modules/ref-napi/build/Release/
+mkdir -p ./KCCacheProxy-win/node_modules/ffi-napi/build/Release
+cp ../node_modules/ffi-napi/build/Release/ffi_bindings.node ./KCCacheProxy-win/node_modules/ffi-napi/build/Release/
 mv proxy-win.exe KCCacheProxy-win/proxy-win.exe &&
 7z a KCCacheProxy-win.zip KCCacheProxy-win &&
 rm -r KCCacheProxy-win &
