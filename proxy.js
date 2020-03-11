6d7874bbbb44979695270d6a3c523ccda96539b5
Tibo <Tibowl@users.noreply.github.com>
2020-03-11 16:53:24 +0100

    Add optional timeout setting

--- proxy.js
+++ proxy.js
@@ -20,7 +20,10 @@ const server = http.createServer(async (req, res) => {
     console.log(method + ": " + url)
 
     if(method !== "GET" || (!KC_PATHS.some(path => url.includes(path))) || url.includes(".php"))
-        return proxy.web(req, res, { target: `http://${req.headers.host}/` })
+        return proxy.web(req, res, {
+            target: `http://${req.headers.host}/`,
+            timeout:  config.timeout
+        })
 
     return await cacher.handleCaching(req, res)
 })
