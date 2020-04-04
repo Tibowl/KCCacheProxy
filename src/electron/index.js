168633e87500fb443fd23cdc888a260ad36b1e87
Tibo <Tibowl@users.noreply.github.com>
2020-04-04 16:17:27 +0200

    Disable devtools

--- src/electron/index.js
+++ src/electron/index.js
@@ -55,7 +55,7 @@ const createWindow = () => {
     mainWindow.loadFile(path.join(__dirname, "index.html"))
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools()
+    // mainWindow.webContents.openDevTools()
 
     mainWindow.setMenu(null)
 
